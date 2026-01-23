be550fa84577432205a9f8e7f8137a017e580d23
ShehabEldeenAyman <shehab.ayman@vito.be>
2026-01-23 11:44:13 +0100

    deprecations and updates

--- deprecated/RiverStage1Year(deprecated).js
+++ deprecated/RiverStage1Year(deprecated).js
@@ -1,25 +1,25 @@
-import { queryGraphDB } from '../../engines/ldesSPARQLengine.js'
+import { queryGraphDB } from '../engines/ldesSPARQLengine.js'
 
-export async function RiverDischarge1Year(req, res) {
+export async function RiverStage1Year(req, res) {
   try {
     const results = await queryGraphDB("http://localhost:7200", "ldes-cache", `
 PREFIX sosa: <http://www.w3.org/ns/sosa/>
 PREFIX ex: <http://example.com/ns#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-SELECT ?subject ?value ?time ?runoffvalue
+SELECT ?subject ?value ?time  
 WHERE {
   GRAPH ?g {
-    ?subject sosa:observedProperty "River Discharge" ;
+    ?subject sosa:observedProperty "River Stage" ;
              sosa:hasSimpleResult ?value ;
-             sosa:resultTime ?time ;
-             ex:runoffValue ?runoffvalue .
+             sosa:resultTime ?time .
     
     # Filter to only include results from the year 2025
     FILTER(YEAR(?time) = 2025)
   }
 }
 ORDER BY DESC(?time)
+
     `);
 
     // Transform the results into a clean array of JSON objects if needed, 
@@ -29,7 +29,7 @@ ORDER BY DESC(?time)
       parameter: observation.parameter,
       value: observation.value,
       time: observation.time,
-      runoffvalue: observation.runoffvalue
+      //runoffvalue: observation.runoffvalue
       //message: `At ${observation.time}, the ${observation.parameter} was ${observation.value} meters.`
     }));
 
